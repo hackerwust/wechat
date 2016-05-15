ab2d3cb4c51b92c5872037e938bd46a8b774bec9
xiaochan <1427182960@qq.com>
2016-05-15 22:29:01 +0800

    初始化wechat项目

---
+++ app/common/config/config.js
@@ -0,0 +1,11 @@
+'use strict';
+/**
+ * config
+ */
+
+exports.__esModule = true;
+exports.default = {
+  //key: value
+  port: 5556
+};
+//# sourceMappingURL=config.js.map
---
+++ app/common/config/view.js
@@ -0,0 +1,17 @@
+'use strict';
+/**
+ * template config
+ */
+
+exports.__esModule = true;
+exports.default = {
+  type: 'jade',
+  content_type: 'text/html',
+  file_ext: '.jade',
+  file_depr: '_',
+  root_path: think.ROOT_PATH + '/view',
+  adapter: {
+    ejs: {}
+  }
+};
+//# sourceMappingURL=view.js.map
